22d04e1115b3ba7fb3de32dd30be020e0aad56d5
bhavana536 <jayabhavanap63@gmail.com>
2026-06-19 11:05:24 +0530

    Add Vercel URL to CORS

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://your-vercel-url.vercel.app"
+        "https://job-tracker-lac-zeta.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
